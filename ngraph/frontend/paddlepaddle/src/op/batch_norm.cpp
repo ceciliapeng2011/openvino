7567962b39ced5778b43196c045896dbcaac1a69
mangguo <mang.guo@intel.com>
2021-05-10 04:44:36 -0400

    Add NHWC support for batch_norm

--- ngraph/frontend/paddlepaddle/src/op/batch_norm.cpp
+++ ngraph/frontend/paddlepaddle/src/op/batch_norm.cpp
@@ -28,8 +28,20 @@ NamedOutputs batch_norm (const NodeContext& node) {
     auto beta = node.get_ng_input("Bias");
     auto mean = node.get_ng_input("Mean");
     auto variance = node.get_ng_input("Variance");
-    return node.default_single_output_mapping({std::make_shared<ngraph::opset6::BatchNormInference>(
-            data, gamma, beta, mean, variance, node.get_attribute<float>("epsilon"))}, {"Y"});
+    auto data_layout = node.get_attribute<std::string>("data_layout");
+    PDPD_ASSERT((data_layout == "NCHW" || data_layout == "NHWC"), "Not supported input data layout!");
+    if(data_layout == "NCHW") {
+        return node.default_single_output_mapping({std::make_shared<ngraph::opset6::BatchNormInference>(
+                data, gamma, beta, mean, variance, node.get_attribute<float>("epsilon"))}, {"Y"});
+    } else if(data_layout == "NHWC") {
+        auto rank = static_cast<unsigned long>(data.get_partial_shape().rank().get_length());
+        auto input_order = ngraph::opset6::Constant::create(ngraph::element::i64, {4}, {0, 3, 1, 2});
+        auto data_nhwc = std::make_shared<ngraph::opset6::Transpose>(data, input_order);
+        auto node_batch_norm = std::make_shared<ngraph::opset6::BatchNormInference>(
+                data_nhwc, gamma, beta, mean, variance, node.get_attribute<float>("epsilon"));
+        auto output_order = ngraph::opset6::Constant::create(ngraph::element::i64, {4}, {0, 1, 2, 3});
+        return node.default_single_output_mapping({std::make_shared<ngraph::opset6::Transpose>(node_batch_norm, output_order)}, {"Y"});
+    }
 }
 
 }}}}
