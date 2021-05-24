7c7d6bb734b2c4fa6e54ba44be654cca219b1f18
jialipen <cecilia.peng@intel.com>
2021-05-24 19:04:52 +0800

    yolo_box/test: add more coverage.

--- ngraph/test/files/paddlepaddle/gen_scripts/generate_yolo_box.py
+++ ngraph/test/files/paddlepaddle/gen_scripts/generate_yolo_box.py
@@ -36,25 +36,39 @@ def yolo_box(name : str, x, img_size, attrs : dict):
 
     return outs
 
-def main():
+def TEST1():
     # yolo_box
     pdpd_attrs = {
+            'name': "yolo_box_default",
             'anchors': [10, 13, 16, 30, 33, 23],
             'class_num': 2,
             'conf_thresh': 0.5,
             'downsample_ratio': 32,
-            'clip_bbox': True, #There is bug in Paddle2ONN where clip_bbox is always ignored.
+            'clip_bbox': False,
             'scale_x_y': 1.0
     }
 
     pdpd_attrs_clip_box = {
+        'name': "yolo_box_clip_box",
         'anchors': [10, 13, 16, 30, 33, 23],
         'class_num': 2,
         'conf_thresh': 0.5,
         'downsample_ratio': 32,
-        'clip_bbox': True, #There is bug in Paddle2ONN where clip_bbox is always ignored.
+        'clip_bbox': True,
         'scale_x_y': 1.0
     }
+
+    pdpd_attrs_scale_xy = {
+        'name': "yolo_box_scale_xy",
+        'anchors': [10, 13, 16, 30, 33, 23],
+        'class_num': 2,
+        'conf_thresh': 0.5,
+        'downsample_ratio': 32,
+        'clip_bbox': True,
+        'scale_x_y': 1.2
+    }
+
+    pdpd_attrs_list = [pdpd_attrs, pdpd_attrs_clip_box, pdpd_attrs_scale_xy]  
 
     N = 32
     num_anchors = int(len(pdpd_attrs['anchors'])//2)
@@ -64,26 +78,56 @@ def main():
     data = np.random.random(x_shape).astype('float32')
     data_ImSize = np.random.randint(10, 20, imgsize_shape).astype('int32') 
 
-    # For any change to pdpd_attrs, do -
-    # step 1. generate paddle model
-    pred_pdpd = yolo_box('yolo_box_test1', data, data_ImSize, pdpd_attrs)
-    #pred_pdpd = yolo_box('yolo_box_clip_box', data, data_ImSize, pdpd_attrs_clip_box)
+    for item in pdpd_attrs_list:
+        pred_pdpd = yolo_box(item['name'], data, data_ImSize, item)
 
-    # step 2. generate onnx model
-    # !paddle2onnx --model_dir=../models/yolo_box_test1/ --save_file=../models/yolo_box_test1/yolo_box_test1.onnx --opset_version=10
-    import subprocess
-    subprocess.run(["paddle2onnx", "--model_dir=../models/yolo_box_test1/", "--save_file=../models/yolo_box_test1/yolo_box_test1.onnx", "--opset_version=12"])
-    #pred_onx = onnx_run(data, data_ImSize, onnx_model="../models/yolo_box_test1/yolo_box_test1.onnx")    
+        # step3.b alternatively, run from frontend API
+        from runtime import OV_frontend_run
+        pred_ie = OV_frontend_run(data, data_ImSize, path_to_model="../models/"+item['name'])
+
+        # step 4. compare 
+        # Try different tolerence
+        from compare_tool import validate
+        validate(pred_pdpd, pred_ie, rtol=1e-4, atol=1e-5)
+
+
+def TEST2():
+    # yolo_box
+    pdpd_attrs = {
+            'name': "yolo_box_uneven_wh",
+            'anchors': [10, 13, 16, 30, 33, 23],
+            'class_num': 2,
+            'conf_thresh': 0.5,
+            'downsample_ratio': 32,
+            'clip_bbox': False,
+            'scale_x_y': 1.0
+    }
+    #
+    # uneven spatial width and height
+    #  
+
+    N = 1
+    SPATIAL_WIDTH = 13
+    SPATIAL_HEIGHT = 9
+    num_anchors = int(len(pdpd_attrs['anchors'])//2)
+    x_shape = (N, num_anchors * (5 + pdpd_attrs['class_num']), SPATIAL_HEIGHT, SPATIAL_WIDTH)
+    imgsize_shape = (N, 2)
+
+    data = np.random.random(x_shape).astype('float32')
+    data_ImSize = np.random.randint(10, 20, imgsize_shape).astype('int32') 
+
+    pred_pdpd = yolo_box(pdpd_attrs['name'], data, data_ImSize, pdpd_attrs)
 
     # step3.b alternatively, run from frontend API
     from runtime import OV_frontend_run
-    pred_ie = OV_frontend_run(data, data_ImSize, path_to_model="../models/yolo_box_test1/")
+    pred_ie = OV_frontend_run(data, data_ImSize, path_to_model="../models/"+pdpd_attrs['name'])
 
     # step 4. compare 
     # Try different tolerence
     from compare_tool import validate
-    validate(pred_pdpd, pred_ie, rtol=1e-4, atol=1e-5) 
+    validate(pred_pdpd, pred_ie, rtol=1e-4, atol=1e-5)         
 
 
 if __name__ == "__main__":
-    main()     
+    #TEST1()
+    TEST2()    
