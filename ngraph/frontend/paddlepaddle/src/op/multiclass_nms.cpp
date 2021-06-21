ca5cf5ff5e04dd9d94dbaf04763081f598eba259
jialipen <cecilia.peng@intel.com>
2021-06-22 00:17:47 +0800

    op: enable multiclass_nms.

--- ngraph/frontend/paddlepaddle/src/op/multiclass_nms.cpp
+++ ngraph/frontend/paddlepaddle/src/op/multiclass_nms.cpp
@@ -3,7 +3,8 @@
 //
 
 #include "multiclass_nms.hpp"
-#include <ngraph/opsets/opset6.hpp>
+#include <ngraph/opsets/opset8.hpp>
+#include <paddlepaddle_frontend/utility.hpp>
 
 namespace ngraph
 {
@@ -15,29 +16,56 @@ namespace ngraph
             {
                 NamedOutputs multiclass_nms(const NodeContext& node)
                 {
+                    using namespace ngraph;
+                    using namespace opset8;
+                    using namespace element;
+
                     auto bboxes = node.get_ng_input("BBoxes");
                     auto scores = node.get_ng_input("Scores");
 
                     auto score_threshold = node.get_attribute<float>("score_threshold");
                     auto iou_threshold = node.get_attribute<float>("nms_threshold");
-                    auto max_output_boxes_per_class = node.get_attribute<int>("nms_top_k");
+                    auto nms_top_k = node.get_attribute<int>("nms_top_k");
+                    auto keep_top_k = node.get_attribute<int>("keep_top_k");
+                    auto background_class = node.get_attribute<int>("background_label");
+                    auto nms_eta = node.get_attribute<float>("nms_eta");
 
-                    // TODO: dtype, scaler/vector attr, and more strick attributes check
-                    auto node_max_output_boxes_per_class = ngraph::opset6::Constant::create<int>(
-                        element::i32, Shape{1}, {max_output_boxes_per_class});
-                    auto node_iou_threshold = ngraph::opset6::Constant::create<float>(
-                        element::f32, Shape{1}, {iou_threshold});
-                    auto node_score_threshold = ngraph::opset6::Constant::create<float>(
-                        element::f32, Shape{1}, {score_threshold});
+                    auto normalized = node.get_attribute<bool>("normalized");
 
-                    return node.default_single_output_mapping(
-                        {std::make_shared<ngraph::opset6::NonMaxSuppression>(
+                    if( !normalized )
+                    {
+                        auto node_value_one = Constant::create<float>(f32, {1}, {1.0});
+
+                        auto node_axis2 = Constant::create<int64_t>(i64, {}, {2});
+                        auto node_new_bboxes = std::make_shared<Split>(bboxes, node_axis2, 4)->outputs();
+                        auto node_new_xmax = std::make_shared<Add>(node_new_bboxes[2], node_value_one);
+                        auto node_new_ymax = std::make_shared<Add>(node_new_bboxes[3], node_value_one);
+
+                        bboxes = std::make_shared<Concat>(OutputVector{node_new_bboxes[0], node_new_bboxes[1], node_new_xmax, node_new_ymax}, 2);
+                    }
+
+                    NamedOutputs named_outputs;
+                    std::vector<Output<Node>> nms_outputs;
+                    nms_outputs = std::make_shared<MulticlassNms>(
                             bboxes,
                             scores,
-                            node_max_output_boxes_per_class,
-                            node_iou_threshold,
-                            node_score_threshold)},
-                        {"Out"});
+                            MulticlassNms::SortResultType::CLASSID,
+                            false,
+                            i64, // TODO
+                            iou_threshold,
+                            score_threshold,
+                            nms_top_k,
+                            keep_top_k,
+                            background_class,
+                            nms_eta)->outputs();
+
+                    auto out_names = node.get_output_names();
+                    PDPD_ASSERT(out_names.size() == 3, "Unexpected number of outputs of MulticlassNMS");
+
+                    named_outputs["Out"] = {nms_outputs[0]};
+                    named_outputs["Index"] = {nms_outputs[1]};
+                    named_outputs["NmsRoisNum"] = {nms_outputs[2]};
+                    return named_outputs;
                 }
 
             } // namespace op
